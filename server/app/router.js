cd2229e31f3619536dd1de4eb1f6fbe1340abfb8
PixelKata <guillaumebasilaire@gmail.com>
2024-10-09 11:41:05 +0200

    :hammer: add action and router for upload

--- server/app/router.js
+++ server/app/router.js
@@ -4,8 +4,10 @@ const router = express.Router();
 
 const authorRouter = require("./router/authorRouter");
 const mangaRouter = require("./router/mangaRouter");
+const uploadRouter = require("./router/uploadRouter");
 
 router.use("/authors", authorRouter);
 router.use("/mangas", mangaRouter);
+router.use("/upload", uploadRouter);
 
 module.exports = router;
